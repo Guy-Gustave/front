74c269274e0992c351fac04e0b6fa9004d4c34f1
Guy <guymoustapha@gmail.com>
2021-06-04 13:47:25 +0200

    add store and configure to dev extensions

---
+++ store.js
@@ -0,0 +1,10 @@
+import { createStore } from 'redux';
+import allReducers from './reducers/index';
+
+const store = createStore(allReducers,
+  {},
+  // eslint-disable-next-line no-underscore-dangle
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+);
+
+export default store;
