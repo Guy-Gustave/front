b9cf2e8efa0fd5cdf63bb6b97b2edf632b409189
Guy <guymoustapha@gmail.com>
2021-07-01 15:03:23 +0200

    add history and exact to login

--- src/Routes.js
+++ src/Routes.js
@@ -1,18 +1,21 @@
+/* eslint-disable */
 import React from 'react';
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import { Router, Switch, Route } from 'react-router-dom';
 import Home from './components/Home';
 import Login from './containers/Login';
 import Register from './containers/Register';
 import ProjectList from './containers/ProjetList';
 import ProjectDetails from './components/ProjectDetails';
+import history from './components/History'
+
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Routes = () => (
-  <Router>
+  <Router history= {history} >
     <Switch>
       <Route exact path="/" component={Home} />
       <Route path="/signup" component={Register} />
-      <Route path="/login" component={Login} />
+      <Route exact path="/login" component={Login} />
       <Route path="/projects" exact component={ProjectList} />
       <Route path="/project/:projectId" exact component={ProjectDetails} />
     </Switch>
