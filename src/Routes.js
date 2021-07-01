40e53362378e3b31b85235258045264142b42fe4
Guy <guymoustapha@gmail.com>
2021-07-01 15:25:11 +0200

    fix eslint errors

--- src/Routes.js
+++ src/Routes.js
@@ -5,12 +5,12 @@ import Login from './containers/Login';
 import Register from './containers/Register';
 import ProjectList from './containers/ProjetList';
 import ProjectDetails from './components/ProjectDetails';
-import history from './components/History'
+// import history from './components/History'
 
 // import 'bootstrap/dist/css/bootstrap.min.css';
 
 const Routes = () => (
-  <Router history= {history} >
+  <Router>
     <Switch>
       <Route exact path="/" component={Home} />
       <Route path="/signup" component={Register} />
